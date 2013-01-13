003b272298e3f5812e99dcfd7af678cd110dbc4c
Sai Suman Cherukuwada <saisuman@localhost>
2013-01-13 01:35:14 +0100

    Added mapping of buffers, C++-ification.

--- streamer/video_streamer.cc
+++ streamer/video_streamer.cc
@@ -2,18 +2,16 @@
 #include <libv4l2.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <string>
 
 #include "log_and_err_util.h"
 #include "video_wrappers.h"
 
+using std::string;
 
 int main(int argc, char **argv) {
   debug("Opening device.");
-  int fd = v4l2_open("/dev/video0", O_RDWR|O_NONBLOCK);
-  CHECK_NONNEG(fd);
-
-  SetCaptureFormat(fd);
-  ok("Set up capture format.");
-  AllocateBuffers(fd, 2);
-  ok("Setup capture buffers.");
+  CaptureSource source("/dev/video0");
+  source.InitOrDie();
+  debug("Initialised.");
 }
--- streamer/video_wrappers.h
+++ streamer/video_wrappers.h
@@ -1,4 +1,20 @@
-bool VideoCommand(int video_dev_fd, int command, void *data);
-void SetCaptureFormat(int video_dev_fd);
-int AllocateBuffers(int video_dev_fd, int num_buffers);
+#include <string>
+#include <vector>
+
+class CaptureSource {
+ public:
+  CaptureSource(const std::string &video_device);
+  ~CaptureSource();
+
+  void InitOrDie();
+
+ private:
+  bool VideoCommand(int command, void *data);
+  void SetCaptureFormat();
+  int AllocateBuffers(int num_buffers);
+  void MapBuffers(int allocated_buffers);
+
+  int fd_;
+  std::vector<std::pair<void *, size_t> > buffers_;
+};
 
